14bef915baa76f1c4359146403dcb3f98eb5aefe
erikvk <erik.von.krusenstierna@gmail.com>
2023-03-04 16:41:09 +0100

    Update uninstall-wpf-client.ps1

--- u/uninstall-wpf-client.ps1
+++ u/uninstall-wpf-client.ps1
@@ -2,7 +2,7 @@ Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
 $ErrorActionPreference = "Stop"
 Write-Host -NoNewline "> Uninstalling WPF Client... "
 $processFilePath = "C:\ProgramData\Heads\Client\bin\PolyjuiceWindows.exe"
-$existingProcess = Get-Process "PolyjuiceWindows" -FileVersionInfo -ErrorAction SilentlyContinue | Where-Object "FileName" -eq $processFilePath
+$existingProcess = Get-Process "PolyjuiceWindows" -ErrorAction SilentlyContinue | Where-Object "Path" -eq $processFilePath
 if ($existingProcess) {
     $existingProcess | Stop-Process -Force
     $existingProcess | Wait-Process
