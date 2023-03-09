0b85be60e4ca33b1e46546a806c4a3801a080068
erikvk <erik.von.krusenstierna@gmail.com>
2023-03-10 00:32:41 +0100

    Also delete desktop shortcut when uninstalling wpfclient

--- u/uninstall-wpf-client.ps1
+++ u/uninstall-wpf-client.ps1
@@ -2,8 +2,14 @@ Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
 $ErrorActionPreference = "Stop"
 Write-Host -NoNewline "> Uninstalling WPF Client... "
 $installDir = "C:\ProgramData\Heads\Client";
-
 $processFilePath = "$installDir\bin\PolyjuiceWindows.exe"
+$shell = New-Object -ComObject WScript.Shell
+$desktopPath = $shell.SpecialFolders("Desktop")
+Get-ChildItem "$desktopPath\*.lnk" | % {
+    if ($shell.CreateShortcut($_).TargetPath -eq $processFilePath) {
+        Remove-Item $_
+    }
+}
 $existingProcess = Get-Process "PolyjuiceWindows" -ErrorAction SilentlyContinue | Where { $_.MainModule.FileName -eq $processFilePath }
 if ($existingProcess) {
     $existingProcess | Stop-Process -Force
