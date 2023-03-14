e3b547c44f6e97461049af06ba90696ec4e28b50
erikvk <erik.von.krusenstierna@gmail.com>
2023-03-14 23:20:48 +0100

    Updated ism

--- install-script-maker.ps1
+++ install-script-maker.ps1
@@ -12,9 +12,10 @@ function Yes
 }
 function Num
 {
-    param($message)
-    $val = Read-Host "$message (number)"
+    param($message, $default)
+    $val = Read-Host $message
     $val = $val.Trim();
+    if ($val -eq '') { return $default }
     if ($val -as [int]) { return $val }
     Write-Host "Invalid value, expected a number"
     return Num $message
@@ -39,38 +40,42 @@ Write-Host
 $environment = Read-Host "> First enter the environment name, e.g. fynda or fynda-test"
 $token = Read-Host "> Now enter the install token" -MaskInput
 $script = @()
+$sideLoad = Yes "> Install as deactivated software in a legacy (SUS/RA) setup for later activation?"
 if (Yes "> Should we first uninstall existing client software, if present?") {
-    $script += "irm raw.githubusercontent.com/byheads/util/main/u/legacy|iex"
+    if (Yes "--> Also uninstall legacy (SUS/RA) client software?") {
+        $script += "irm raw.githubusercontent.com/byheads/util/main/u/legacy|iex"
+    }
     $script += "irm raw.githubusercontent.com/byheads/util/main/u/all|iex"
 }
 if (Yes "> Install Receiver?") {
-    $script += "irm `"`$uri/product=Receiver`" -Headers `$hds|iex"
+    $script += "irm `"`$u/product=Receiver`" @o|iex"
 }
 if (Yes "> Install WpfClient?") {
     $part = "product=WpfClient"
-    if (Yes "--> Install deactivated in a legacy setup for later activation?") {
+    if ($sideLoad) {
         $part += "&sideLoad=True"
     }
     $part += "&usePosServer=" + (Yes "--> Connect client to local POS Server?")
     $part += "&useArchiveServer=" + (Yes "--> Connect client to central Archive Server?")
-    $script += "irm `"`$uri/$part`" -Headers `$hds|iex"
+    $script += "irm `"`$u/$part`" @o|iex"
 }
 if (Yes "> Install POS Server?") {
     $part = "product=PosServer"
-    if (Yes "--> Install deactivated in a legacy setup for later activation?") {
+    if ($sideLoad) {
         $part += "&sideLoad=True"
     } else {
         $part += "&createDump=" + (Yes "--> Create a dump of an existing POS-server?")
     }
-    $part += "&databaseImageSize=" + (Num "--> Enter database image size in MB")
-    $part += "&databaseLogSize=" + (Num "--> Enter database log size in MB")
+    $part += "&databaseImageSize=" + (Num "--> Enter database image size in MB (or enter for 1024)" 1024)
+    $part += "&databaseLogSize=" + (Num "--> Enter database log size in MB (or enter for 1024)" 1024)
     $part += "&collation=" + (Collation "--> Enter database collation, e.g. sv-SE")
-    $script += "irm `"`$uri/$part`" -Headers `$hds|iex"
+    $script += "irm `"`$u/$part`" @o|iex"
 }
+$throwSeparator = ";if(!`$?){throw};"
 Write-Host
 Write-Host "# Here's your install script! Run it in PowerShell as administrator on a client computer:"
 Write-Host
-$script | Join-String -Separator ";if(!`$?){throw};" | % { "`$hds=@{Authorization=`"Bearer $token`"};`$uri=`"https://broadcaster.$environment.heads-api.com/api/install`";$_" } | Out-Host
+$script | Join-String -Separator $throwSeparator | % { "`$o=@{Headers=@{Authorization=`"Bearer $token`"}};`$u=`"https://broadcaster.$environment.heads-api.com/api/install`";$_" } | Out-Host
 Write-Host
 Write-Host "# End of script"
 Write-Host
