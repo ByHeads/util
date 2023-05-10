6acbd8ae859c6f4174422f0a193ac687dbadaa84
erikvk <erik.von.krusenstierna@gmail.com>
2023-05-10 22:24:57 +0200

    Fixes for scripts

---
+++ u/src/uninstall-customer-service-application.ps1
@@ -0,0 +1,32 @@
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
+$ErrorActionPreference = "Stop"
+Write-Host -NoNewline "> Uninstalling Customer Service Application... "
+$installDir = "C:\ProgramData\Heads\CustomerServiceApplication";
+$processFilePath = "$installDir\bin\PolyjuiceWindows.exe"
+$shell = New-Object -ComObject WScript.Shell
+$desktopPath = $shell.SpecialFolders("Desktop")
+Get-ChildItem "$desktopPath\*.lnk" | % {
+    if ($shell.CreateShortcut($_).TargetPath -eq $processFilePath) {
+        Remove-Item $_
+    }
+}
+$desktopPath = "$env:Public\Desktop"
+Get-ChildItem "$desktopPath\*.lnk" | % {
+    if ($shell.CreateShortcut($_).TargetPath -eq $processFilePath) {
+        Remove-Item $_
+    }
+}
+$existingProcess = Get-Process "PolyjuiceWindows" -ErrorAction SilentlyContinue | Where { $_.MainModule.FileName -eq $processFilePath }
+if ($existingProcess) {
+    $existingProcess | Stop-Process -Force
+    $existingProcess | Wait-Process
+    Start-Sleep -Seconds 4
+}
+$exists = Test-Path $installDir
+rm -r $installDir -ErrorAction SilentlyContinue
+
+if ($exists) {
+    echo "$((Get-Date -AsUTC).ToString("yyyyMMddHHmmss") ): UNINSTALLED Customer Service Application" >> "C:\ProgramData\Heads\install.log"
+    Write-Host "Done!"
+}
+else { Write-Host "No installation found" }
