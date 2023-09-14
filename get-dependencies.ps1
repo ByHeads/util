05ee08af4e28174622f1cb17d34ec4dfe3aac0a5
erikvk <erik.von.krusenstierna@gmail.com>
2023-09-14 13:29:30 +0200

    Update get-dependencies.ps1

--- get-dependencies.ps1
+++ get-dependencies.ps1
@@ -10,21 +10,21 @@ pwsh {
     Write-Host
     Read-Host "Press Enter to continue or Ctrl+C to abort"
     $bcPath = Read-Host "> Enter the Broadaster install directory path"
-    $o = New-Item -Path "$bcPath\bcman" -ItemType Directory -ErrorAction SilentlyContinue
-    $o = New-Item -Path "$bcPath\vcredist" -ItemType Directory -ErrorAction SilentlyContinue
-    $o = New-Item -Path "$bcPath\powershell" -ItemType Directory -ErrorAction SilentlyContinue
+    $o = ni "$bcPath\bcman" -ItemType Directory -ErrorAction SilentlyContinue
+    $o = ni "$bcPath\vcredist" -ItemType Directory -ErrorAction SilentlyContinue
+    $o = ni "$bcPath\powershell" -ItemType Directory -ErrorAction SilentlyContinue
     $powershellUrl = irm https://api.github.com/repos/PowerShell/PowerShell/releases/latest `
-    | % { $_.assets } `
-    | ? { $_.browser_download_url -like "*win-x64.msi*" } `
-    | select -exp browser_download_url
+      | % { $_.assets } `
+      | ? { $_.browser_download_url -like "*win-x64.msi*" } `
+      | select -exp browser_download_url
     $powershellFileName = Split-Path -Path $powershellUrl -Leaf
     Write-Host "> Downloading $powershellFileName from $powershellUrl"
-    irm $powershellUrl -OutFile "$bcPath\powershell\$powershellFileName"
+    irm $powershellUrl -o "$bcPath\powershell\$powershellFileName"
     Write-Host "> Downloading bcman.ps1 from https://raw.githubusercontent.com/byheads/bcman/main/bcman.ps1"
-    irm raw.githubusercontent.com/byheads/bcman/main/bcman.ps1 -OutFile "$bcPath\bcman\bcman.ps1"
+    irm raw.githubusercontent.com/byheads/bcman/main/bcman.ps1 -o "$bcPath\bcman\bcman.ps1"
     Write-Host "> Downloading vcredist x64 from https://aka.ms/vs/17/release/vc_redist.x64.exe"
-    irm https://aka.ms/vs/17/release/vc_redist.x64.exe -OutFile "$bcPath\vcredist\vc_redist.x64.exe"
+    irm https://aka.ms/vs/17/release/vc_redist.x64.exe -o "$bcPath\vcredist\vc_redist.x64.exe"
     Write-Host "> Downloading vcredist x86 from https://aka.ms/vs/17/release/vc_redist.x86.exe"
-    irm https://aka.ms/vs/17/release/vc_redist.x86.exe -OutFile "$bcPath\vcredist\vc_redist.x86.exe"
+    irm https://aka.ms/vs/17/release/vc_redist.x86.exe -o "$bcPath\vcredist\vc_redist.x86.exe"
     Write-Host "> Done!" -ForegroundColor Green
 }
