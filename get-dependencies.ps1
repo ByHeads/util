f4fbc072b88b61789d1ffc0f84af417286067b96
erikvk <erik.von.krusenstierna@gmail.com>
2023-09-14 13:14:36 +0200

    Update get-dependencies.ps1

--- get-dependencies.ps1
+++ get-dependencies.ps1
@@ -1,18 +1,31 @@
-$bcPath = Read-Host "> BC install dir: "
-New-Item -Path "$bcPath\bcman" -ItemType Directory
-New-Item -Path "$bcPath\vcredist" -ItemType Directory
-New-Item -Path "$bcPath\powershell" -ItemType Directory
-$powershellUrl = irm https://api.github.com/repos/PowerShell/PowerShell/releases/latest `
+pwsh {
+    Write-Host "This script will download and install:"
+    Write-Host
+    Write-Host "- PowerShell 7 from it's official Microsoft source"
+    Write-Host "- bcman.ps1 from it's GitHub repo"
+    Write-Host "– Visual C++ Redistributable 2015-2022 x64 and x86 the official Microsoft source"
+    Write-Host
+    Write-Host "The files will be placed in subdirectories to the Broadcaster install directory,"
+    Write-Host "from where they can be deployed by the Broadcaster to clients"
+    Write-Host
+    Read-Host "Press Enter to continue or Ctrl+C to abort"
+    Write-Host
+    $bcPath = Read-Host "> Enter the Broadaster install directory path"
+    $o = New-Item -Path "$bcPath\bcman" -ItemType Directory
+    $o = New-Item -Path "$bcPath\vcredist" -ItemType Directory
+    $o = New-Item -Path "$bcPath\powershell" -ItemType Directory
+    $powershellUrl = irm https://api.github.com/repos/PowerShell/PowerShell/releases/latest `
     | % { $_.assets } `
     | ? { $_.browser_download_url -like "*win-x64.msi*" } `
     | select -exp browser_download_url
-Write-Host "> Downloading $powershellFileName from $powershellUrl"
-$powershellFileName = Split-Path -Path $powershellUrl -Leaf
-irm $powershellUrl -OutFile "$bcPath\powershell\$powershellFileName"
-Write-Host "> Downloading bcman.ps1 from https://raw.githubusercontent.com/byheads/bcman/main/bcman.ps1"
-irm raw.githubusercontent.com/byheads/bcman/main/bcman.ps1 -OutFile "$bcPath\bcman\bcman.ps1"
-Write-Host "> Downloading vcredist x64 from https://aka.ms/vs/17/release/vc_redist.x64.exe"
-irm https://aka.ms/vs/17/release/vc_redist.x64.exe -OutFile "$bcPath\vcredist\vc_redist.x64.exe"
-Write-Host "> Downloading vcredist x86 from https://aka.ms/vs/17/release/vc_redist.x86.exe"
-irm https://aka.ms/vs/17/release/vc_redist.x86.exe -OutFile "$bcPath\vcredist\vc_redist.x86.exe"
-Write-Host "Done!"
+    $powershellFileName = Split-Path -Path $powershellUrl -Leaf
+    Write-Host "> Downloading $powershellFileName from $powershellUrl"
+    irm $powershellUrl -OutFile "$bcPath\powershell\$powershellFileName"
+    Write-Host "> Downloading bcman.ps1 from https://raw.githubusercontent.com/byheads/bcman/main/bcman.ps1"
+    irm raw.githubusercontent.com/byheads/bcman/main/bcman.ps1 -OutFile "$bcPath\bcman\bcman.ps1"
+    Write-Host "> Downloading vcredist x64 from https://aka.ms/vs/17/release/vc_redist.x64.exe"
+    irm https://aka.ms/vs/17/release/vc_redist.x64.exe -OutFile "$bcPath\vcredist\vc_redist.x64.exe"
+    Write-Host "> Downloading vcredist x86 from https://aka.ms/vs/17/release/vc_redist.x86.exe"
+    irm https://aka.ms/vs/17/release/vc_redist.x86.exe -OutFile "$bcPath\vcredist\vc_redist.x86.exe"
+    Write-Host "Done!"
+}
